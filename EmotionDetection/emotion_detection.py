d765db54f6bedc4e73dd7854bb9600f2ba29d0e0
David Medina <david00medina@gmail.com>
2025-08-19 13:16:03 -0400

    Bis - Task 7: Incorporate Error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -11,6 +11,13 @@ def emotion_detector(text_to_analyse) -> Dict:
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     payload = {"raw_document": { "text": text_to_analyse }}
     res = requests.post(url,headers=headers,json=payload)
+
+    if res.status_code == 400:
+        return {
+            "anger": None, "disgust": None, "fear": None, "joy": None, "sadness": None,
+            "dominant_emotion": None
+        }
+
     res_json = json.loads(res.text)
 
     # Transform the response into the requested output format
--- server.py
+++ server.py
@@ -9,6 +9,10 @@ def emotion_detector_endpoint():
     text_to_analyze = request.args.get("textToAnalyze")
     print(text_to_analyze)
     emotions_detected = emotion_detector(text_to_analyze)
+
+    if emotions_detected.get("dominant_emotion") is None:
+        return "Invalid text! Please try again!", 200
+
     return render_template(
         'system_response.html',
         anger_score=emotions_detected.get('anger'), 
